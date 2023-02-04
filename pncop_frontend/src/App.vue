24181e51d23cfa05cd8d4112a749924912d5f0ed
mufiye <mufiye140520@gmail.com>
2023-02-04 18:49:35 +0800

    setup change

--- pncop_frontend/src/App.vue
+++ pncop_frontend/src/App.vue
@@ -9,7 +9,8 @@
       空间参与度阈值：<input type="text" placeholder="请输入空间参与度阈值" v-model="mining_parameters.spatialPrev"><br>
       时间参与度阈值：<input type="text" placeholder="请输入时间参与度阈值" v-model="mining_parameters.temporalPrev">
     </form>
-    <el-button type="primary" @click="submitInfo()">计算频繁项集</el-button>
+    <el-button type="primary">Primary</el-button>
+    <button type="submit" @click="submitInfo()">计算频繁项集</button>
     <p>Positive Set: {{ result1 }}</p>
     <p>Negative Set: {{ result2 }}</p>
   </div>
@@ -18,32 +19,29 @@
 <script>
 // import { computeFrequentSet } from './api/api.js'
 import axiosInstance from './api/index'
+import { ref } from 'vue';
 const axios = axiosInstance
 
 export default { // 组件暴露的属性
   name: 'App',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      mining_parameters: {
-        "disThreshold": "",
-        "spatialPrev": "",
-        "temporalPrev": "",
-        "csvFile":'',
-        "csvFileName":''
-      },
-      result1: '',
-      result2: ''
-    }
-  },
-  methods: {
-    submitInfo() {
+  setup() {
+    let msg = ref('Welcome to Your Vue.js App');
+    let mining_parameters = ref({
+      "disThreshold": "",
+      "spatialPrev": "",
+      "temporalPrev": "",
+      "csvFile":'',
+      "csvFileName":''
+    });
+    let result1 = ref('');
+    let result2 = ref('');
+    function submitInfo() {
       let param = new FormData()
-      param.append("disThreshold", this.mining_parameters.disThreshold)
-      param.append("spatialPrev", this.mining_parameters.spatialPrev)
-      param.append("temporalPrev", this.mining_parameters.temporalPrev)
-      param.append("csvFile", this.mining_parameters.csvFile)
-      param.append("csvFileName", this.mining_parameters.csvFileName)
+      param.append("disThreshold", mining_parameters.value.disThreshold)
+      param.append("spatialPrev", mining_parameters.value.spatialPrev)
+      param.append("temporalPrev", mining_parameters.value.temporalPrev)
+      param.append("csvFile", mining_parameters.value.csvFile)
+      param.append("csvFileName", mining_parameters.value.csvFileName)
       console.log(param.get("csvFile"))
       // console.log(this.mining_parameters.disThreshold)
       // console.log(this.mining_parameters.spatialPrev)
@@ -51,18 +49,56 @@ export default { // 组件暴露的属性
       // console.log(this.mining_parameters.csvFile)
       // console.log(this.mining_parameters.csvFileName)
       axios.post('http://localhost:8000/api/pncop/', param).then(response => {
-        this.result1 = response.data['positiveMsg']
-        this.result2 = response.data['negativeMsg']
+        result1.value = response.data['positiveMsg']
+        result2.value = response.data['negativeMsg']
       })
-    },
-    handleGetFile(e) {
-      console.log("in handleGetFile function")
-      this.mining_parameters.csvFile = e.target.files[0]
-      this.mining_parameters.csvFileName = e.target.files[0].name
     }
-  }
-  // created: function () {
+    function handleGetFile(e) {
+      console.log("in handleGetFile function")
+      mining_parameters.value.csvFile = e.target.files[0]
+      mining_parameters.value.csvFileName = e.target.files[0].name
+    }
+    return { msg, mining_parameters, result1, result2, submitInfo, handleGetFile }
+  },
+  // data() {
+  //   return {
+  //     msg: 'Welcome to Your Vue.js App',
+  //     mining_parameters: {
+  //       "disThreshold": "",
+  //       "spatialPrev": "",
+  //       "temporalPrev": "",
+  //       "csvFile":'',
+  //       "csvFileName":''
+  //     },
+  //     result1: '',
+  //     result2: ''
+  //   }
+  // },
 
+  // methods: {
+  //   submitInfo() {
+  //     let param = new FormData()
+  //     param.append("disThreshold", this.mining_parameters.disThreshold)
+  //     param.append("spatialPrev", this.mining_parameters.spatialPrev)
+  //     param.append("temporalPrev", this.mining_parameters.temporalPrev)
+  //     param.append("csvFile", this.mining_parameters.csvFile)
+  //     param.append("csvFileName", this.mining_parameters.csvFileName)
+  //     console.log(param.get("csvFile"))
+  //     // console.log(this.mining_parameters.disThreshold)
+  //     // console.log(this.mining_parameters.spatialPrev)
+  //     // console.log(this.mining_parameters.temporalPrev)
+  //     // console.log(this.mining_parameters.csvFile)
+  //     // console.log(this.mining_parameters.csvFileName)
+  //     axios.post('http://localhost:8000/api/pncop/', param).then(response => {
+  //       this.result1 = response.data['positiveMsg']
+  //       this.result2 = response.data['negativeMsg']
+  //     })
+  //   },
+  //   handleGetFile(e) {
+  //     console.log("in handleGetFile function")
+  //     this.mining_parameters.csvFile = e.target.files[0]
+  //     this.mining_parameters.csvFileName = e.target.files[0].name
+  //   }
   // }
 }
 </script>
--- pncop_frontend/src/main.js
+++ pncop_frontend/src/main.js
@@ -3,6 +3,6 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-const app = createApp(App)
+let app = createApp(App)
 app.use(ElementPlus)
 app.mount('#app')
